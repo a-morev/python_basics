6e3cb5edd08b67fb4b730bcac8bdbd9ff26e4a70
a-morev <ion05pk@gmail.com>
2019-12-11 21:06:14 +0300

    Переделано 6 задиние 5 урока

--- lesson_5/hw6.py
+++ lesson_5/hw6.py
@@ -6,10 +6,21 @@
 предмета и общее количество занятий по нему. Вывести словарь на экран.
 """
 
-with open('for_hw6.txt', 'r', encoding='utf-8') as init_f:
-    subj = {}
-    for line in init_f:
-        subject, lecture, practice, labs = line.split()
-        subj[subject] = int(lecture) + int(practice) + int(labs)
+# Функция делит список по символам и суммирует цифры из этого списка
 
-    print(subj)
+
+def sum_from_string(value: str):
+    value_split = (val.split('(') for val in value.split(' '))
+    value_digit = (int(val) for val_ar in value_split for val in val_ar if val.isdigit())
+    return sum(value_digit)
+
+
+with open('for_hw6.txt', 'r', encoding='utf-8') as file:
+    lessons = file.readlines()
+    # Генератор, делящий строки по разделителю
+    lessons_split = (line.split(':') for line in lessons)
+    # Собираем словарь
+    lessons = {
+        lesson_name: sum_from_string(les_hours) for lesson_name, les_hours in lessons_split
+    }
+    print(lessons)
